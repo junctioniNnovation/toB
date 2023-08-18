2510ed8c3bf7e1bec80ae805a3eb37c1f04c47dc
tkgka <ksh17171@gmail.com>
2023-08-19 06:37:06 +0900

    [SETTING] : initial commit

---
+++ toB/toB/toBApp.swift
@@ -0,0 +1,17 @@
+//
+//  toBApp.swift
+//  toB
+//
+//  Created by 김수환 on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct toBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
