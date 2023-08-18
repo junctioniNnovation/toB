95acacfa24ce6e7e5d1d1a059cc51cce842d6e12
tkgka <ksh17171@gmail.com>
2023-08-19 06:55:45 +0900

    [SETTING] : add appDelegate

---
+++ toB/toB/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  toB
+//
+//  Created by 김수환 on 2023/08/19.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+}
--- toB/toB/toBApp.swift
+++ toB/toB/toBApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct toBApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
