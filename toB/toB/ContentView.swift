707ca2ddf04b97f7e49ae5883de7616a9ef2c535
김다빈 <mokdb17@gmail.com>
2023-08-20 07:09:10 +0900

    [FEAT] 경로 초기버전 UI (#15)

--- toB/toB/ContentView.swift
+++ toB/toB/ContentView.swift
@@ -50,6 +50,7 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
 
 struct UIMapView: UIViewRepresentable {
     @Binding var coord: CLLocationCoordinate2D
+    let pathOverlay = NMFPath()
     
     func makeUIView(context: Context) -> NMFNaverMapView {
         let view = NMFNaverMapView()
@@ -66,5 +67,16 @@ struct UIMapView: UIViewRepresentable {
         cameraUpdate.animation = .fly
         cameraUpdate.animationDuration = 1.0
         uiView.mapView.moveCamera(cameraUpdate)
+        
+        pathOverlay.path = NMGLineString(points: [
+            NMGLatLng(lat:35.159260, lng: 129.170146),
+            NMGLatLng(lat:35.161112, lng: 129.171540),
+            NMGLatLng(lat:35.156361, lng: 129.177946),
+            NMGLatLng(lat:35.164246, lng: 129.185680),
+            NMGLatLng(lat:35.162257, lng: 129.187934),
+            NMGLatLng(lat:35.160306, lng: 129.191395)
+
+        ])
+        pathOverlay.mapView = uiView.mapView
     }
 }
