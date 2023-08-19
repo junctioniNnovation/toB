a80429aa33ad128d1a0e1e6e6d09d8d9ecbf1ef5
김다빈 <mokdb17@gmail.com>
2023-08-20 02:30:59 +0900

    [Feat] 길찾기 api를 사용하기 위한 데이터 모델 수정(#19)

--- toB/toB/Views/SearchViews/SearchModel.swift
+++ toB/toB/Views/SearchViews/SearchModel.swift
@@ -9,10 +9,15 @@
 import Foundation
 import SwiftUI
 
+
+// MARK: 기존의 모델에서 위도와 경도를 옵셔널로 추가했어요 위도와 경도를 추가함으로써 길찾기 api를 통해 빠른 비교가 가능해졌습니다.
 struct PlaceInfo: Identifiable,Hashable {
-    // 더미데이터 모델
     let id = UUID()
     var placeName: String
     var place: String
     var distance: String
+    var longtitude : Double?
+    var latitude : Double?
 }
+
+
