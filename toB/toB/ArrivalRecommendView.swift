5f2bb0cfa4beaeb12a7c60dcd346c90dbc4bde6c
NewYorkKim <newyorkkim1030@gmail.com>
2023-08-19 16:12:48 +0900

    [FEAT] Add ArrivalRecommendView

---
+++ toB/toB/ArrivalRecommendView.swift
@@ -0,0 +1,24 @@
+//
+//  ArrivalRecommentView.swift
+//  toB
+//
+//  Created by Nayeon Kim on 2023/08/19.
+//
+
+import SwiftUI
+
+struct ArrivalRecommentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                ContentView()
+            }
+        }
+    }
+}
+
+struct ArrivalRecommentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArrivalRecommentView()
+    }
+}
